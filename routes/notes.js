97438dd3cb992f24730f403d608bb831d58634a4
rackeelBrooks <rackeel13@gmail.com>
2022-05-09 17:03:59 -0500

    corrections to routes

--- routes/notes.js
+++ routes/notes.js
@@ -13,6 +13,7 @@ router.get('/edit/:id' , function(req,res,next){
       title : 'Proj Notes - Edit Notes',
       stylesheet : '/stylesheets/edit-note.css',
       edit : results[0],
+      dateT : new Date(results[0].active_date).toLocaleString()
     }
 
     res.render('notes/edit-note', locals)
@@ -42,20 +43,50 @@ router.get('/edit/:id' , function(req,res,next){
 // })
 
 router.post('/update', function(req, res, next) {
-      
-  let sqlQuery = "UPDATE notes SET note ='" + req.body.note_info + 
+  let sqlQuery = "UPDATE softwareProj.notes SET note ='" + req.body.note_info + 
                                       "', active_date ='" + req.body.active_date + 
-                                      "' WHERE id = " + req.body.id;
+                                      "' WHERE id = " + req.body.project_id;
 
   connection.query(sqlQuery, function(err,rows)     {
-  
+    var locals ={
+      title : 'proj',
+      stylesheet:'/stylesheets/projects.css'
+    }
+
              //req.flash('error', err); 
-             res.redirect('/projects/more' + req.body.project_id);   
-             next();                
+             res.redirect('/projects/more/'+ req.body.project_id)
+           
           });
          
      });
 
+     router.get('/notes-form/:id', function(req,res){
+      var locals = {
+        title : 'Proj Notes - Add Note',
+        stylesheet : '/stylesheets/projects.css',
+        params : req.params.id
+      }
+      res.render('notes/add-note', locals)
+    })
+
+
+    router.post('notes/added' , function(req,res,next){
+  let data = {
+    note: req.body.note_info,
+    active_date: req.body.active_date,
+    project_id: req.body.project_id,
+
+  }
+
+  let insertQuery = "INSERT INTO notes SET ?"
+
+  connection.query(insertQuery, data, (err,results)=>{
+    if(err)throw err
+    console.log(JSONResponse(results));
+    res.redirect('/projects/more/'+ req.body.project_id)
+    next()
+    })
+})
 
 
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/more/:id' , function(req, res, next) {
 
-  let notesQuery = "SELECT pj.project_title, pj.project_description , pj.project_start_dt, pj.project_due_dt , nt.note, nt.active_date , nt.project_id , nt.id  FROM  softwareProj.projects pj , softwareProj.notes nt WHERE nt.project_id = pj.id AND nt.project_id ="
+  let notesQuery = "SELECT pj.project_title, pj.project_description , pj.project_start_dt, pj.project_due_dt , pj.id, nt.note, nt.active_date , nt.project_id , nt.id  FROM  softwareProj.projects pj , softwareProj.notes nt WHERE nt.project_id = pj.id AND nt.project_id ="
 
   connection.query( notesQuery + req.params.id , (err,results) => {
     if (err) throw err.toString();
@@ -31,6 +31,7 @@ router.get('/more/:id' , function(req, res, next) {
       title : 'Proj Notes - Show Projects',
       stylesheet : '/stylesheets/projects.css',
       notes : results,
+      params : req.params.id
     }
 
 
@@ -38,6 +39,36 @@ router.get('/more/:id' , function(req, res, next) {
   })
 })
 
+router.get('/project-forms', function(req,res,next){
+  var locals = {
+    title : 'Proj Notes - Add Project',
+    stylesheet : '/stylesheets/projects.css'
+  }
+  res.render('projects/add_project', locals)
+})
 
+
+router.post('/project-forms/add' , function(req,res,next){
+  let data = {
+    project_title: req.body.project_title,
+    project_description: req.body.project_description,
+    project_start_dt: req.body.start_dt,
+    project_due_dt: req.body.due_dt
+  }
+
+  let insertQuery = "INSERT INTO projects SET ?"
+
+  connection.query(insertQuery, data, (err,results)=>{
+    if(err)throw err
+    console.log(JSONResponse(results));
+    res.redirect('/projects')
+    next()
+    })
+})
+
+
+function JSONResponse(results, err_code=200, err_msg='null'){
+  return JSON.stringify({"status_code": err_code, "error": err_msg, "message": results});
+}
 
 module.exports = router;
